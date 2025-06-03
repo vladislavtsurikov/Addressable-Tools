64763aeaa0facbd4cc49f5103484a805ff96a2fc
Vladislav Tsurikov <uladzislau78@gmail.com>
2025-06-03 23:27:25 +0300

    [Add] SerializedCollections plugin

---
+++ Assets/Plugins/SerializedCollections/Sample/SerializedDictionarySample.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace AYellowpaper.SerializedCollections
+{
+    public class SerializedDictionarySample : MonoBehaviour
+    {
+        [SerializedDictionary("Element Type", "Description")]
+        public SerializedDictionary<ElementType, string> ElementDescriptions;
+        
+        public enum ElementType
+        {
+            Fire,
+            Air,
+            Earth,
+            Water
+        }
+    }
+}
